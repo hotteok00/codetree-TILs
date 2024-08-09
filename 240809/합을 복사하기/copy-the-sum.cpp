16b6cd15988c692aafc4f11e336a2cba06d6a8a3
hotteok00 <112681531+hotteok00@users.noreply.github.com>
2024-08-09 22:06:36 +0900

    Title: 합을 복사하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240809/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    int sum = a + b + c;
+    a = sum;
+    b = sum;
+    c = sum;
+
+    printf("%d %d %d", a, b, c);
+
+    return 0;
+}
